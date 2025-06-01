918c0e4f69dac919a1f601c7f6fb69e6c31868e1
Tomasz Krolikowski <tomasz.krolikowski@vnet.com.pl>
2025-06-01 16:43:08 +0000

    feat(frontend): wyświetlanie zadań z API i stylowanie listy

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,21 +1,32 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5001/tasks') // https://app.krolikowski.cloud/tasks 
-      .then(res => res.json())
-      .then(data => setTasks(data));
+    console.log("useEffect start");
+
+    fetch('https://app.krolikowski.cloud/tasks')
+      .then(response => {
+        console.log("Odpowiedź serwera:", response);
+        return response.json();
+      })
+      .then(data => {
+        console.log("Odebrane dane:", data);
+        setTasks(data);
+      })
+      .catch(error => console.error("Błąd fetcha:", error));
   }, []);
 
   return (
-    <div>
+    <div className="app">
       <h1>Lista zadań</h1>
       <ul>
         {tasks.map(task => (
-          <li key={task.id}>{task.title}</li>
+          <li key={task.id}>
+            <strong>{task.title}</strong>: {task.description}
+          </li>
         ))}
       </ul>
     </div>
